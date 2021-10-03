dbf6c46aca925186818f9d7021667f1e4d9971e3
BIgra77 <44897812+BIgra77@users.noreply.github.com>
2021-10-03 18:06:47 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ComboBox1.SelectedIndex == 0)
+                {
+                    Output.Text = cipher.Cesar(Input.Text);
+                }
+                else if (ComboBox1.SelectedIndex == 1)
+                {
+                    Output.Text = cipher.Binary(Input.Text);
+                }
+                else if (ComboBox1.SelectedIndex == 2)
+                {
+                    Output.Text = cipher.VigenèreCipher(Input.Text,Key.Text);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Il y a un problème :  " + exception.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            
+            
+        }
+        
+    }
+}
---
+++ cipher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace WpfApp1
+{
+    public class cipher
+    {
+        public static char OnClick(string text)
+        {
+            char input = text [1];
+            return input;
+        }
+        
+        public static string Cesar(string text)
+        {
+            
+            byte[] ASCIIvalues = Encoding.ASCII.GetBytes(text);
+            var NewText= new List<char>();
+            foreach(var value in ASCIIvalues)
+            {
+                int NewVal = value + 1;
+                char c = (char) NewVal;
+                NewText.Add(c);
+            }
+            string NewString = string.Join("", NewText.ToArray());
+            return NewString;
+        }
+
+        public static string Binary(string text)
+        {
+            StringBuilder Binaire = new StringBuilder();
+
+            foreach (char carca in text)
+            {
+                Binaire.Append(Convert.ToString(carca, 2).PadLeft(8, '0'));
+            }
+            return Binaire.ToString();
+        }
+        
+        public static string VigenèreCipher(string text, string key)
+        {
+            string first_key = "azertyuiopqsdfghjklmwxcvbn";
+            text=text.ToLower();
+            var NewText= new List<char>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                int ValLettre = first_key.IndexOf(text[i]);
+                int ValInKey = first_key.IndexOf(key[i]);
+                int ValFinale = (ValLettre + ValInKey) % 26;
+                NewText.Add(first_key[ValFinale]);
+            }
+            string NewString = string.Join("", NewText.ToArray());
+            return NewString;
+        }
+    }
+}
